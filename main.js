ebe88990e68e8b4a569d8589d8d732e555ea87d4
windstudio286 <90179866+windstudio286@users.noreply.github.com>
2022-03-31 16:03:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ var app = express()
  
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello world => Welcome to my home by CôngTT.')
+  res.send('<b>Hello world </b> => Welcome to my home by CôngTT.')
 })
 app.get('/healthcheck', function (req, res) {
-  res.send('Ok,Hi')
+  res.send('Ok.')
 })
 app.listen(8081, function () {
   console.log('app listening on port 8081!')
